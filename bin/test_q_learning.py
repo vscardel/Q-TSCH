fd1c9599156f915687e8ec58079d1e9f37fb2fd7
vscardel <vscardel@gmail.com>
2023-09-08 19:24:56 -0300

    test wihh good conditions

--- bin/test_q_learning.py
+++ bin/test_q_learning.py
@@ -5,33 +5,22 @@ if __name__ == '__main__':
 
     UPPER_LIMIT = 11
     FIXED_VALUE = 0.2
-    NUM_PARAM = 3
   
-    for i in range(NUM_PARAM):
+    for ALFA in range(1,UPPER_LIMIT,2):
 
-        for param in range(1,UPPER_LIMIT,2):
-            param = param / 10
-            if i == 0:
-                ALFA = param
-                BETA = 0.2
-                EPSLON = 0.2
-            elif i == 1:
-                BETA = param
-                ALFA = 0.2
-                EPSLON = 0.2
-            else:
-                EPSLON = param
-                ALFA = 0.2
-                EPSLON = 0.2
+        ALFA = float(ALFA)/10
+
+        for BETA in range(1,UPPER_LIMIT,2):
+
+            BETA = float(BETA)/10
 
             with open("config.json", "r") as jsonFile:
                 config_data = json.load(jsonFile)
                 config_data["settings"]['regular']['ALFA'] = ALFA
                 config_data["settings"]['regular']['BETA'] = BETA
-                config_data["settings"]['regular']['EPSLON'] = EPSLON
         
             with open("config.json", "w") as jsonFile:
                 json.dump(config_data, jsonFile)
 
-            print("Rodando Iteracao ALFA = {0}, BETA = {1} E EPSLON = {2}".format(ALFA,BETA,EPSLON))
+            print("Rodando Iteracao ALFA = {0}, BETA = {1}".format(ALFA,BETA))
             rc = subprocess.call("python2 runSim.py", shell=True)
