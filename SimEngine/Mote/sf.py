change location of adaptation and fix epslon computation

SimEngine/Mote/sf.py
@@ -251,7 +251,11 @@ class SchedulingFunctionMSF(SchedulingFunctionBase):
                         }
                     )
                     self.tx_cell_utilization = tx_cell_utilization
-                # self._adapt_to_traffic(preferred_parent, self.TX_CELL_OPT)
+
+                self.mote.tsch.EPSLON = self.mote.tsch.MIN_EPSLON + (self.mote.tsch.MAX_EPSLON - self.mote.tsch.MIN_EPSLON)*np.exp(-self.mote.tsch.EPSLON_DECAY_RATE*self.mote.tsch.CURRENT_EPISODE)
+                self.mote.tsch.CURRENT_EPISODE = self.mote.tsch.CURRENT_EPISODE + 1
+
+                self._adapt_to_traffic(preferred_parent, self.TX_CELL_OPT)
                 self._reset_cell_counters(self.TX_CELL_OPT)
 
 
@@ -562,7 +566,7 @@ class SchedulingFunctionMSF(SchedulingFunctionBase):
         return 1
     
     def discretize_dropped_packets(self,dropped_packets):
-        if dropped_packets <= 0.3:
+        if dropped_packets <= 0.1:
             return 0
         return 1
      
@@ -620,8 +624,10 @@ class SchedulingFunctionMSF(SchedulingFunctionBase):
         # print(self.mote.tsch.dropped_packets)
         # print('--------------------')
 
-    def _adapt_to_traffic(self, neighbor, cell_opt,is_training):
+    def _adapt_to_traffic(self, neighbor, cell_opt):
         # reset retry counter
+        print("EPSLON")
+        print(self.mote.tsch.EPSLON)
         assert neighbor in self.retry_count
         if self.retry_count[neighbor] != -1:
             # we're in the middle of a 6P transaction; try later

SimEngine/Mote/tsch.py
@@ -90,13 +90,13 @@ class Tsch(object):
         self.INTERVAL = 10
         self.dropped_packets = 0
         self.average_dropped_packets_in_interval = 0
-        self.CURRENT_EPISODE = -1
+        self.CURRENT_EPISODE = 0
         self.asn_is_synced = 0
 
         self.EPSLON = None
         self.MAX_EPSLON = 1.0           
         self.MIN_EPSLON = 0.05           
-        self.EPSLON_DECAY_RATE = 0.0005  
+        self.EPSLON_DECAY_RATE = 0.005  
 
         self.IS_TRAINING = True
 
@@ -1029,14 +1029,12 @@ class Tsch(object):
 
                 preferred_parent = self.mote.rpl.getPreferredParent()
 
-                if preferred_parent:
+                # if preferred_parent:
                     #comeco de um novo episodio
-                    self.EPSLON = self.MIN_EPSLON + (self.MAX_EPSLON - self.MIN_EPSLON)*np.exp(-self.EPSLON_DECAY_RATE*self.CURRENT_EPISODE+1)
-                    self.CURRENT_EPISODE = self.CURRENT_EPISODE + 1
 
                     #step
-                    print("Chamando adaptacao no slotframe " + str(slotframe_count) + " " + str(self.mote.id ))
-                    self.mote.sf._adapt_to_traffic(preferred_parent,self.mote.sf.TX_CELL_OPT,is_training = self.IS_TRAINING)
+                    # print("Chamando adaptacao no slotframe " + str(slotframe_count) + " " + str(self.mote.id ))
+                    # self.mote.sf._adapt_to_traffic(preferred_parent,self.mote.sf.TX_CELL_OPT,is_training = self.IS_TRAINING)
             else:
                 #is in the same slotframe. Do nothing
                 pass