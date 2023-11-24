adc9e62ea26b7f1abc93efd056db9e64dbeca05d
vscardel <vscardel@gmail.com>
2023-11-24 16:13:49 -0300

    msf 10

--- bin/plot_same_graph.py
+++ bin/plot_same_graph.py
@@ -46,12 +46,12 @@ def main(options):
     subfolders = list(
         [os.path.join(options.inputfolder, x) for x in os.listdir(options.inputfolder)]
     )
-    subfolder = "simData/ComparisonResults100"
+    subfolder = "simData/ComparisonResults50"
 
     for key in options.kpis:
         # load data
-        file_path_msf = "simData/MSF100/exec_numMotes_100.dat.kpi"
-        file_path_qlearning = 'simData/Q-Learning100/exec_numMotes_100.dat.kpi'
+        file_path_msf = "simData/MSF50/exec_numMotes_50.dat.kpi"
+        file_path_qlearning = 'simData/Q-learning50/exec_numMotes_50.dat.kpi'
             
         curr_combination = "exec_numMotes_50"
 
